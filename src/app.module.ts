7ce95382b940e69773bdf1146393c7df41563db3
Ing. Cristian Cueto Vargas <ccuetovargas65@gmail.com>
2022-09-02 18:00:24 -0400

    conexion a una base de datos mysql e inicalizacion del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './apis/api.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ApiModule],
+  imports: [
+    ApiModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      name: 'proyect',
+      host: process.env.PROYECT_HOST,
+      port: parseInt(process.env.PROYECT_PORT, 10),
+      username: process.env.PROYECT_USER,
+      password: process.env.PROYECT_PASSWORD,
+      database: process.env.PROYECT_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
